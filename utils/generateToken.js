f7b3c28048726ef927ebc0cccf6b1ff4c5ba46c4
Ishita Joshi <ishita27joshi@gmail.com>
2026-09-01 04:26:36 +0530

    cookie sameSite none + secure for cross-origin

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -11,7 +11,8 @@ const generateToken = (id, res) => {
     res.cookie("token", token, {
         httpOnly: true,
         maxAge: 10 * 24 * 60 * 60 * 1000,
-        sameSite: "strict",
+        sameSite: "none",
+        secure: true,
     })
 
 };
